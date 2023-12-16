7cb0c714e002bcc4cdaadceb7ddd28073bae9c4e
Yash Dafade <yashdafade93@gmail.com>
2023-12-16 13:31:30 +0530

    about section changes

--- portfolio/src/Components/Aboutme.jsx
+++ portfolio/src/Components/Aboutme.jsx
@@ -17,10 +17,10 @@
                             </div>
                             <div class="column right">
                                 <div data-aos="fade-up" class="text">I'm Yash Dafade</div>
-                                <p data-aos="fade-up">An aspiring Web-Developer seeking an entry-level opportunity with an esteemed organization where I can utilize my skills & enhance learning in the field of work. Capable of mastering new technologies.</p>
+                                <p data-aos="fade-up">Aspiring web developer seeking entry-level opportunities with esteemed organizations. Eager to apply and enhance my skills while mastering new technologies in the dynamic field of web development.</p>
                                 <br />
                                 <div data-aos="fade-up" class="text">Why Work With Me?</div>
-                                <p data-aos="fade-up">My goal is to always deliver pixel perfect, responsive and interactive websites. I am a team player and always ready to learn new things. I am a hardworking person and always ready to face challenges.</p>
+                                <p data-aos="fade-up"> I'm a skilled web developer proficient in <b>HTML, CSS, and JavaScript</b>, with expertise in frameworks like <b>Bootstrap and React.js</b>. My strong grasp of version control using <b>Git and GitHub</b> ensures efficient collaboration. Committed to staying current with industry trends, I bring a passion for continuous learning. As a dedicated team player, I deliver <b>pixel-perfect</b> websites that exceed expectations. Let's transform your ideas into exceptional digital experiences!</p>
                             </div>
                         </div>
                     </div>
